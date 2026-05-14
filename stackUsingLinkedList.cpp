748a374376474c73e1a2e019fb95a3db1174fed6
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-05-14 11:27:50 +0700

    logika perulangan (while) untuk menampilkan semua elemen stack

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -53,3 +53,9 @@ public:
         {
             cout << "List is empty." << endl;
         }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
